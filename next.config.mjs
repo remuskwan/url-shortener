e5403907216e5a72c13b3fc99e0567756233039a
Remus <remuskwan23@gmail.com>
2023-08-14 00:42:52 +0800

    fix: cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -10,20 +10,12 @@ const ContentSecurityPolicy = `
   font-src 'self' https: data:;
   form-action 'self';
   frame-ancestors 'self';
-  img-src 'self' data: blob: ${
-    // For displaying images from R2
-    env.R2_PUBLIC_HOSTNAME ? `https://${env.R2_PUBLIC_HOSTNAME}` : ''
-  };
+  img-src 'self' data: blob: ;
   frame-src 'self';
   object-src 'none';
   script-src 'self' ${env.NODE_ENV === 'production' ? '' : "'unsafe-eval'"};
   style-src 'self' https: 'unsafe-inline';
-  connect-src 'self' https://*.browser-intake-datadoghq.com https://vitals.vercel-insights.com/v1/vitals ${
-    // For POSTing presigned URLs to R2 storage.
-    env.R2_ACCOUNT_ID
-      ? `https://*.${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`
-      : ''
-  };
+  connect-src 'self' https://*.browser-intake-datadoghq.com https://vitals.vercel-insights.com/v1/vitals ;
   worker-src 'self' blob:;
   ${env.NODE_ENV === 'production' ? 'upgrade-insecure-requests' : ''}
 `
@@ -45,7 +37,7 @@ const config = {
   /** We run eslint as a separate task in CI */
   eslint: { ignoreDuringBuilds: !!process.env.CI },
   images: {
-    domains: [env.R2_PUBLIC_HOSTNAME ?? ''].filter((d) => d),
+    domains: [],
   },
   async headers() {
     return [

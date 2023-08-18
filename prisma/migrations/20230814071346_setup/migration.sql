a6078b775acc6bf8498772b60b82e7839ef70ece
Remus <remuskwan23@gmail.com>
2023-08-18 13:26:19 +0800

    fix: format function

---
+++ prisma/migrations/20230814071346_setup/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `qr_code` on the `URL` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "URL" DROP COLUMN "qr_code";
